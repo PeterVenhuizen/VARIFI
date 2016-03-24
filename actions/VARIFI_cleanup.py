ca1bcc50db81c1179f7cac6c86073fe332160d15
VENHUIZEN Peter <peterv_@fitch.cibiv.univie.ac.at>
2016-03-24 17:21:41 +0100

    Script for cronjob cleaning

--- actions/VARIFI_cleanup.py
+++ actions/VARIFI_cleanup.py
@@ -1,24 +1,18 @@
 #!/usr/bin/env python
 
+import subprocess
 import MySQLdb
 from VARIFI_functions import FetchOneAssoc
 
 # Connect to MySQL DB
-db = MySQLdb.connect('localhost', 'root', 'root', 'VARIFI')
+db = MySQLdb.connect('gray', 'varifi', 'v4r1f1us3r', 'varifi')
 cursor = db.cursor()
 
-sql = 'SELECT * FROM job_info WHERE job_info.job_token IN (SELECT submitted_jobs.job_token FROM submitted_jobs WHERE available_until > CURRENT_TIMESTAMP AND finished)'
+sql = 'SELECT job_token FROM submitted_jobs WHERE available_until < CURRENT_TIMESTAMP AND finished'
 cursor.execute(sql)
 row = FetchOneAssoc(cursor)
 while row is not None:
 
-	token = row['job_token']
-	files = [row['read_file'], row['bed_file'], row['extra_file']]
-	
-	# Remove job files
-	
-	# Remove job result files
-	
-	# Update progress message
+	subprocess.call('lynx -dump http://varifi.cibiv.univie.ac.at/actions/remove.php?token={} &> /dev/null'.format(row['job_token']), shell=True)
 	
 	row = FetchOneAssoc(cursor)	# Next row
